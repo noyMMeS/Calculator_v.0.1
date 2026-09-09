a02eb3350137c656403d69006a57d7c7d254329a
25300282 <25300282@K216W12>
2026-09-09 12:21:51 +0500

    Добавьте файлы проекта.

---
+++ Calculator/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введите первое число"); double num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите второе число"); double num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите действие (+,-,*,/)"); string operation = Console.ReadLine();
+if (operation == "+")
+{
+    Console.WriteLine(num1 + num2);
+}
+else if (operation == "-")
+{
+    Console.WriteLine(num1 - num2);
+}
+else if (operation == "*")
+{
+    Console.WriteLine(num1 * num2);
+}
+
+else if (operation == "/")
+{
+    Console.WriteLine(num1 / num2);
+}
+else
+{
+    Console.WriteLine("Ошибочка");
+} 
+
